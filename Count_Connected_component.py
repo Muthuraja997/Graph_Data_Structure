70a6e7e9d560b49a50434b0f944104ee61f3a781
Muthuraja997 <muthuraja05980@gmail.com>
2025-07-12 09:33:38 +0530

    connected component

---
+++ Count_Connected_component.py
@@ -0,0 +1,36 @@
+n = 8
+edges = [
+    [0, 1], [1, 2], [2, 0],     
+    [3, 4], [4, 5], [5, 3]      
+]
+
+
+l = set(frozenset([i]) for i in range(n))
+
+
+for st, en in edges:
+    start = end = None
+    for comp in l:
+        if st in comp:
+            start = comp
+        if en in comp:
+            end = comp
+        if start and end:
+            break
+
+    if start != end:
+        l.remove(start)
+        l.remove(end)
+        l.add(frozenset(start.union(end)))
+
+
+for st, en in edges:
+    for comp in l:
+        if st in comp and en in comp:
+            print("YES")
+            break
+    else:
+        continue
+    break
+
+print(len(l)) 
